391dfb55d14b816a712ed0457d4824f8579a4b4d
João Victor Ramiro <85127729+Jvramiro@users.noreply.github.com>
2026-03-19 17:12:10 -0300

    Feat Task Service to interact with API

---
+++ frontend/task-manager-app/src/app/services/task.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../environments/environment";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Task, TaskCreateDTO } from "../models/task.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class TaskService {
+    private apiUrl = `${environment.apiUrl}/task`;
+
+    constructor(private http: HttpClient) {}
+
+    getAll(): Observable<Task[]> {
+        return this.http.get<Task[]>(this.apiUrl);
+    }
+
+    getById(id: number): Observable<Task> {
+        return this.http.get<Task>(`${this.apiUrl}/${id}`);
+    }
+
+    create(task: TaskCreateDTO): Observable<Task> {
+        return this.http.post<Task>(this.apiUrl, task);
+    }
+
+    update(id: number, task: TaskCreateDTO): Observable<Task> {
+        return this.http.put<Task>(`${this.apiUrl}/${id}`, task);
+    }
+
+    delete(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+}
